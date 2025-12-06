f0701099e3ef2a310b018ddc925874d8bfc38ac6
Omar Khlaif <omarpc164@gmail.com>
2025-12-06 18:54:40 +0200

    Add comments to explain main logic

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -121,6 +121,7 @@ def user_stats(df):
 
 
 def main():
+    """Run the bikeshare interactive analysis program."""
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
